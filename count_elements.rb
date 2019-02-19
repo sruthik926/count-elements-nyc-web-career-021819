8dccccb9c064ef2e121eee07943442411b8f5170
github username <github email address>
2019-02-19 12:49:18 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
+  new_Hash = {}
+
+  array.each do |elem|
+    if new_Hash[elem]
+       new_Hash[elem] += 1
+    else
+       new_Hash[elem] = 1
+   end
   # code goes here
+  end
+      new_Hash
 end
- 
